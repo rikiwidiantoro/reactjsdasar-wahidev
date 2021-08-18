9c4e604a6c5ee3285ef0d81a67d9887aa1a4cd0d
Riki-Toro <rikitoro22@gmail.com>
2021-08-18 18:06:14 +0700

    variabel

--- src/index.js
+++ src/index.js
@@ -2,11 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 // import './index.css';
 import App from './App';
+import Variabel from './variabel/Variabel';
 // import reportWebVitals from './reportWebVitals';
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <Variabel />
   </React.StrictMode>,
   document.getElementById('root')
 );
---
+++ src/variabel/Variabel.js
@@ -0,0 +1,28 @@
+// // const
+// const harga = 3000
+
+// let
+// let harga = 3000
+// if(true) {
+//     let harga = 30
+// }
+
+// var
+var harga = 3000
+if(true) {
+    var harga = 30
+}
+
+
+
+
+const Variabel = () => {
+    return (
+        <div>
+            <h1>Belajar Variabel</h1>
+            <p>harga : {harga}</p>
+        </div>
+    )
+}
+
+export default Variabel;
