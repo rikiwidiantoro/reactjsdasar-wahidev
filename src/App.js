f84300a1e75a9129563455de79d3e695cd148f5f
Riki-Toro <rikitoro22@gmail.com>
2021-08-18 16:47:01 +0700

    komponen baru

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 // import logo from './logo.svg';
 // import './App.css';
+import Navbar from './komponen/Navbar';
+
+// // pakai arrow component
+// const Navbar = () => {
+//   return (
+//     <div>
+//       <nav>
+//         <h3>ini adalah Navbar</h3>
+//       </nav>
+//     </div>
+//   )
+// }
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <h2>Hello World!</h2>
     </div>
   );
---
+++ src/komponen/Navbar.js
@@ -0,0 +1,11 @@
+
+
+
+// function component
+function Navbar() {
+    return (
+        <h4>ini adalah teks Navbar</h4>
+    )
+}
+
+export default Navbar
