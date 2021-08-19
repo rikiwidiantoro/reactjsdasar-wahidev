f3ed7fefdcef0a3002bcc43c98cd1fa7152b27e6
Riki-Toro <rikitoro22@gmail.com>
2021-08-19 13:21:01 +0700

    state

--- src/StateProps/StateProps.js
+++ src/StateProps/StateProps.js
@@ -1,10 +1,26 @@
 import { Component } from "react";
 
 class StateProps extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            makanan : 'Bakso'
+        }
+    }
+
+    GantiMakanan(makananBaru) {
+        this.setState({
+            makanan: makananBaru
+        })
+    }
+
     render() {
         return (
             <div>
                 <h1>Hello World!</h1>
+                <p>Nama makanan = <strong>{this.state.makanan}</strong></p>
+                {/* <button onClick={()=> this.setState({makanan:'Soto'})}>Ganti Makanan</button> */}
+                <button onClick={()=> this.GantiMakanan('Soto')}>Ganti Makanan</button>
             </div>
         )
     }
