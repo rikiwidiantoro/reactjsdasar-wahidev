c2f62f06a62743c20c0c290d93080dff4314c5ed
Riki-Toro <rikitoro22@gmail.com>
2021-08-21 19:02:20 +0700

    create

--- src/crud/index.js
+++ src/crud/index.js
@@ -5,13 +5,37 @@ import Navebar from "./komponen/Navbar";
 import Tabel from "./komponen/Tabel";
 
 class Crud extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            makanans : [],
+            nama : "",
+            deskripsi : "",
+            harga : 0,
+            id : ""
+        }
+    }
+
+    handleChange = (event) => {
+        this.setState({
+            [event.target.name] : event.target.value
+        })
+    }
+
+    handleSubmit = (event) => {
+        event.preventDefault();
+
+        console.log('Data : ', this.state)
+    }
+
     render() {
         return (
             <div>
                 <Navebar />
                 <div className="container mt-3">
                     <h2 className="text-center">CRUD Sederhana dengan React Js</h2>
-                    <Formulir />
+                    <Formulir {...this.state} handleChange={this.handleChange} handleSubmit={this.handleSubmit} />
                     <Tabel />
                 </div>
                 <Footer />
--- src/crud/komponen/Formulir.js
+++ src/crud/komponen/Formulir.js
@@ -1,7 +1,7 @@
 import React from "react";
 import {Row, Col, Form, Button} from 'react-bootstrap'
 
-const Formulir = () => {
+const Formulir = ({nama, deskripsi, harga, handleSubmit, handleChange}) => {
     return (
         <div className="mt-4">
             <Row>
@@ -12,20 +12,20 @@ const Formulir = () => {
             </Row>
             <Row>
                 <Col>
-                    <Form>
+                    <Form onSubmit={(event) => handleSubmit(event)}>
                         <Form.Group className="mb-3" controlId="formBasicEmail">
                             <Form.Label>Nama Makanan</Form.Label>
-                            <Form.Control type="text" name="nama" autoComplete="off" />
+                            <Form.Control type="text" name="nama" autoComplete="off" value={nama} onChange={(event) => handleChange(event)} />
                         </Form.Group>
                         
                         <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
                             <Form.Label>Deskripsi Makanan</Form.Label>
-                            <Form.Control as="textarea" rows={3} name="deskripsi" autoComplete="off" />
+                            <Form.Control as="textarea" rows={3} name="deskripsi" autoComplete="off" value={deskripsi} onChange={(event) => handleChange(event)} />
                         </Form.Group>
 
                         <Form.Group className="mb-3" controlId="formBasicPassword">
                             <Form.Label>Harga</Form.Label>
-                            <Form.Control type="number" name="harga" autoComplete="off" />
+                            <Form.Control type="number" name="harga" autoComplete="off" value={harga} onChange={(event) => handleChange(event)} />
                         </Form.Group>
 
                         <Button variant="dark" type="submit">
